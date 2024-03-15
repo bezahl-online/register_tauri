a2fd6fbb8d533a2bb86c797bf2b6029f68568e64
Ralph Eichelberger <ralph.eichelberger@gmail.com>
2024-03-15 17:42:01 +0100

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,3 +8,19 @@ fn main() {
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+
+// fn main() {
+//   tauri::Builder::default()
+//       .setup(|app| {
+//           #[cfg(debug_assertions)] // only include this code on debug builds
+//           {
+//               let window = app.get_window("main").unwrap();
+
+//               window.open_devtools();
+//               window.close_devtools();
+//           }
+//           Ok(())
+//       })
+//       .run(tauri::generate_context!())
+//       .expect("error while running tauri application");
+// }
